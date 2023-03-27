8dc1f81bf967367da1045fec36c026e156c5444d
东澪 <1908459261@qq.com>
2023-03-27 23:14:56 +0800

    第6周作业

---
+++ src/Sixteenth/Book.java
@@ -0,0 +1,17 @@
+package Sixteenth;
+
+/**
+ * Author:AixCert
+ * Date:2023/3/27 23:10
+ */
+public class Book {
+    String Bname;//书名
+    String Author;//作者
+    String publicer;//出版社
+    double price;//价格
+
+
+    public void SeeBook() {
+        System.out.println("正在看书");
+    }
+}
---
+++ src/Sixteenth/Phone.java
@@ -0,0 +1,23 @@
+package Sixteenth;
+
+import java.util.Date;
+
+/**
+ * Author:AixCert
+ * Date:2023/3/27 18:56
+ */
+public class Phone {
+    String brand;//品牌
+    double price;//价格
+    double ScreenSize;//屏幕尺寸
+    String cpu;//处理器
+
+    public void call() {
+        System.out.println("手机正在打电话");
+    }
+
+    public void playGame() {
+        System.out.println("手机在玩游戏");
+    }
+
+}
