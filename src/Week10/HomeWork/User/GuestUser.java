8395eb777b6ecd74455e5e1aabd2107f57420a43
东澪 <1908459261@qq.com>
2023-05-11 13:24:08 +0800

    完善业务逻辑

--- src/Week10/HomeWork/User/GuestUser.java
+++ src/Week10/HomeWork/User/GuestUser.java
@@ -1,11 +1,13 @@
 package Week10.HomeWork.User;
 
 /**
+ * 游客类
  * Author:AixCert
  * Date:2023/4/26 22:25
  */
 public class GuestUser extends User {
+    // 构造方法
     public GuestUser(String username) {
-        super(username, "游客用户");
+        super(username, "游客");
     }
 }
--- src/Week10/HomeWork/User/RealNameUser.java
+++ src/Week10/HomeWork/User/RealNameUser.java
@@ -1,22 +1,49 @@
 package Week10.HomeWork.User;
 
 /**
+ * 实名用户类
  * Author:AixCert
  * Date:2023/4/26 22:25
  */
 public class RealNameUser extends User {
-    private String password;
-    private String realName;
-    private String phoneNum;
-    private String idCard;
-    private String gender;
+    private String password; // 密码
+    private String realName; // 真实姓名
+    private String phoneNumber; // 电话号码
+    private String idNumber; // 身份证号码
+    private String gender; // 性别
 
-    public RealNameUser(String username, String password, String realName, String phoneNum, String idCard, String gender) {
+    // 构造方法
+    public RealNameUser(String username, String password, String realName, String phoneNumber, String idNumber, String gender) {
         super(username, "普通用户");
         this.password = password;
         this.realName = realName;
-        this.phoneNum = phoneNum;
-        this.idCard = idCard;
+        this.phoneNumber = phoneNumber;
+        this.idNumber = idNumber;
         this.gender = gender;
+    }
+
+    // 获取密码
+    public String getPassword() {
+        return password;
+    }
+
+    // 获取真实姓名
+    public String getRealName() {
+        return realName;
+    }
+
+    // 获取电话号码
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // 获取身份证号码
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    // 获取性别
+    public String getGender() {
+        return gender;
     }
 }
